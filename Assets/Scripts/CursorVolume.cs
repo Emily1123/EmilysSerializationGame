fe1e8ce61861838cf2468f12c7058a4250e846da
Drew Castalia <dcastalia@pima.edu>
2018-11-07 11:52:21 -0700

    Cursor Volume script set up

---
+++ Assets/Scripts/CursorVolume.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorVolume : MonoBehaviour {
+
+	public void OnTriggerEnter( Collider col ) {
+		Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+	}
+
+	public void OnTriggerExit( Collider col ) {
+		Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = false;
+	}
+}
