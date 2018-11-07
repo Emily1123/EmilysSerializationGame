4690d870e4e10d969b5a94c47a9f7393c589e85c
Emily Garcia <egarcia83@mail.pima.edu>
2018-11-07 06:54:12 -0700

    Added script to manage soda machine UI

---
+++ Assets/Scripts/MachineMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MachineMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    //GameObject currentHover;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerCurrentRaycast.gameObject != null)
+        {
+            //currentHover = eventData.pointerCurrentRaycast.gameObject;
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = false;
+        //currentHover = null;
+    }
+}
